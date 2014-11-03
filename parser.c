0ee3488fe61a067855bb0ecdd8e0ac54ccfa5c09
Tom Beckmann <tomjonabc@gmail.com>
2014-11-03 01:37:23 +0100

    Forgot to add changes to parser

--- parser.c
+++ parser.c
@@ -4,25 +4,61 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#define PAGE_SIZE 512
+
 typedef enum
 {
 	NONE,
+	HEADER,
 
-	ASTERIK,
-	UNDERSCORE,
-	SHARP,
-	EQUAL,
+	BOLD,
+	UNDERLINE,
+	ITALIC,
 
-	BRACKET_OPEN,
-	BRACKET_CLOSE,
+	LIST_ITEM,
+	UNORDERED_LIST_ITEM,
+
+	HEADING_1,
+	HEADING_2,
+	HEADING_3,
+	HEADING_4,
+	HEADING_5,
+	HEADING_6,
+
+	LINK,
+	CODE,
+	MULTILINE_CODE,
 	MATH
 } Token;
 
+typedef struct {
+	Token type;
+	char *data;
+	int len;
+} Element;
+
+#define is_whitespace(c) ((c) == ' ' || (c) == '\t' || c == '\n')
+
+#define skip_whitespaces(c) \
+	while (is_whitespace(*c)) c++
+
 int
-parse (int fd, int len)
+parse (int fd, int len, char *error)
 {
 	char *c;
 	Token current_token;
+	Element *list;
+	Element *current_element;
+	int is_start_of_line = 1;
+
+#define next_element() current_element++; \
+	if (!current_element) \
+		exit (1);
+
+	list = calloc (PAGE_SIZE, sizeof (Element));
+	current_element = list;
+
+	current_element->type = NONE;
 
 	c = (char *) mmap (NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
 
@@ -30,12 +66,133 @@ parse (int fd, int len)
 		return 0;
 
 	do {
-		// printf ("%c\n", *c);
+		if (is_start_of_line)
+			skip_whitespaces (c);
+
 		switch (*c) {
+			case '{':
+				if (!is_start_of_line)
+					break;
+
+				current_element->type = HEADER;
+				current_element->data = c;
+
+				// just assume header and skip for now
+				while (*(++c) != '}') {
+					if (!*c) {
+						error = "Header not terminated";
+						return 0;
+					}
+
+					current_element->len++;
+				}
+
+				next_element ();
+				break;
 			case '*':
+				printf ("IS START OF LINE: %i\n", is_start_of_line);
+				if (is_start_of_line) {
+					current_element->type = LIST_ITEM;
+					c++;
+					skip_whitespaces (c);
+					next_element ();
+				} else {
+					current_element->type = BOLD;
+					current_element->data = c + 1;
+
+					while (*(++c) != '*') {
+						if (!*c) {
+							error = "Bold not terminated";
+							return 0;
+						}
+
+						current_element->len++;
+					}
+
+					next_element ();
+				}
+
+				break;
+			case '`':
+				if (c[1] == '`' && c[2] == '`') {
+					current_element->type = MULTILINE_CODE;
+					c += 2;
+
+					current_element->data = c;
+					while (!(*c == '`' && c[1] == '`' && c[2] == '`'))
+						current_element->len++;
+				} else {
+					current_element->type = CODE;
+					current_element->data = c + 1;
+
+					while (*++c != '`') {
+						if (!*c) {
+							error = "Code literal not terminated";
+							return 0;
+						}
+
+						current_element->len++;
+					}
+				}
+
+				next_element ();
+				break;
+			case '$':
+				current_element->type = MATH;
+				current_element->data = c + 1;
+
+				while (*++c != '$') {
+					if (!*c) {
+						error = "Math literal not terminated";
+						return 0;
+					}
+
+					current_element->len++;
+				}
+
+				next_element ();
+				break;
+			case '#':
+				if (is_start_of_line) {
+					current_element->type = HEADING_1;
+					while (*++c == '#')
+						current_element->type++;
+
+					skip_whitespaces (c);
+
+					current_element->data = c;
+					while (*(++c) != '\n')
+						current_element->len++;
+
+					next_element ();
+				}
+				break;
+			case '_':
+				current_element->type = ITALIC;
+				current_element->data = c + 1;
+
+				while (*(++c) != '_') {
+					if (!*c) {
+						error = "Italic not terminated";
+						return 0;
+					}
+
+					current_element->len++;
+				}
+
+				next_element ();
+				break;
+			case '\n':
 				break;
 		}
+
+		is_start_of_line = *c == '\n';
 	} while (*(++c));
+
+	Element *i;
+	for (i = list; i != current_element; i++) {
+		printf ("%i, %.*s\n", i->type, i->len, i->data);
+	}
 
 	return 1;
 }
@@ -46,6 +203,7 @@ main (int argc, char **argv)
 	char *path;
 	int fd;
 	struct stat st;
+	char error[512];
    
 	path = argv[1];
 
@@ -64,7 +222,7 @@ main (int argc, char **argv)
 		exit (1);
 	}
 
-	if (!parse (fd, st.st_size)) {
+	if (!parse (fd, st.st_size, error)) {
 		perror ("Parsing failed");
 		exit (1);
 	}
